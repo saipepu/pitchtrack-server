c472f4694895390fc2930cf7d7e956409137ccbc
Han Htet Aung <juzbird11@gmail.com>
2024-08-12 09:31:55 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { TransformInterceptor } from './transform.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformInterceptor())
+
+  const config = new DocumentBuilder().build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      operationsSorter: 'method',
+    },
+  });
+
+
   await app.listen(8080);
 }
 bootstrap();
