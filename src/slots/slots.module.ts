dd5f0d0db9bcd843cec311ce6ec215b3861ba654
Han Htet Aung <juzbird11@gmail.com>
2024-08-15 21:51:12 +0700

    enable exception filter and interceptor in slot module

--- src/slots/slots.module.ts
+++ src/slots/slots.module.ts
@@ -3,10 +3,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SlotSchema } from './schemas/slot.schema';
 import { SlotController } from './slots.controller';
 import { SlotService } from './slots.service';
+import { TransformInterceptor } from 'src/interceptors/transform.interceptor';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionsFilter } from 'src/filters/all-exceptions.filter';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Slot', schema: SlotSchema }])],
   controllers: [SlotController],
-  providers: [SlotService]
+  providers: [SlotService,
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: TransformInterceptor
+  },
+  {
+    provide: APP_FILTER,
+    useClass: AllExceptionsFilter
+  }
+]
 })
 export class SlotsModule {}
