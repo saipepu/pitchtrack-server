f80003ed4b8f424d5513020c86c641927b3ba3f7
Han Htet Aung <juzbird11@gmail.com>
2024-08-16 11:22:17 +0700

    add swagger ApiResponse helper

---
+++ src/helpers/swagger.helper.ts
@@ -0,0 +1,34 @@
+import { getSchemaPath } from '@nestjs/swagger';
+
+
+export function ApiSuccessResponseHelper(schema: string, array?: 'array') {
+    if (array) {
+        return {
+            status: 200, schema: {
+            properties: {
+            success: { type: 'boolean', example: true},
+            message: { type: 'array', items: {$ref: getSchemaPath(schema)} }
+            },
+        }}
+    }
+
+    return { 
+        status: 200, 
+        schema: {
+            properties: {
+            success: { type: 'boolean', example: true},
+            message: { type: 'string', $ref: getSchemaPath(schema)}
+        },
+    }}
+}
+
+
+export function ApiNotSucessResponseHelper() {
+    return { 
+        status: 400, schema: {
+        properties: {
+            success: { type: 'boolean', example: false},
+            message: { type: 'string', example: 'Internal server error'}
+        },
+    }}
+}
