4e316f91f8b28d4ebab41b000d206d59f3467744
Han Htet Aung <juzbird11@gmail.com>
2024-08-18 08:59:49 +0700

    add message service and schema in event module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -5,13 +5,16 @@ import { EventController } from './events.controller';
 import { EventSchema } from './schemas/event.schema';
 import { SlotService } from 'src/slots/slots.service';
 import { SlotSchema } from 'src/slots/schemas/slot.schema';
+import { MessagesService } from 'src/messages/messages.service';
+import { MessageSchema } from 'src/messages/schemas/message.schema';
 
 @Module({
   imports: [
   MongooseModule.forFeature([{ name: 'Event', schema: EventSchema }]), 
   MongooseModule.forFeature([{ name: 'Slot', schema: SlotSchema}]),
+  MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema}]),
 ],
   controllers: [EventController],
-  providers: [EventService, SlotService],
+  providers: [EventService, SlotService, MessagesService],
 })
 export class EventsModule {}
