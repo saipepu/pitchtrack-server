b66bda54c82dcc66f86bbb7ddb31ebcd52967dde
Han Htet Aung <juzbird11@gmail.com>
2024-08-19 19:28:36 +0700

    add swagger in events

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,21 +1,25 @@
 
-import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { Slot } from '../../slots/schemas/slot.schema'
 import { Message } from 'src/messages/schemas/message.schema';
 import { Type } from 'class-transformer';
 import { CreateSlotDto } from 'src/slots/dto/create-slot.dto';
 import { CreateMessageDto } from 'src/messages/dto/create-message.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateEventDto {
+    @ApiProperty({ example: "Event1"})
     @IsString()
     @IsNotEmpty()
     readonly title: string;
 
+    @ApiPropertyOptional({ type: CreateSlotDto, isArray: true})
     @ValidateNested({ each: true })
     @Type(() => CreateSlotDto)
     readonly slots: Slot[];
 
-    @ValidateNested()
+    @ApiPropertyOptional({ type: CreateMessageDto, isArray: true})
+    @ValidateNested({ each: true})
     @Type(() => CreateMessageDto)
     readonly messages: Message[];
 }
--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -3,9 +3,10 @@ import { EventService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { CreateSlotDto } from 'src/slots/dto/create-slot.dto';
-import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { ApiExtraModels, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateMessageDto } from 'src/messages/dto/create-message.dto';
 import { Event } from './schemas/event.schema';
+import { ApiNotSucessResponseHelper, ApiSuccessResponseHelper } from 'src/helpers/swagger.helper';
 
 @ApiExtraModels(Event)
 @ApiTags('Events')
@@ -13,36 +14,50 @@ import { Event } from './schemas/event.schema';
 export class EventController {
   constructor(private readonly EventService: EventService) {}
 
+  @ApiResponse(ApiSuccessResponseHelper(CreateEventDto.name))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Post()
   create(@Body() createEventDto: CreateEventDto) {
     return this.EventService.create(createEventDto);
   }
 
+  @ApiResponse(ApiSuccessResponseHelper(Event.name, 'array'))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Get()
   findAll() {
     return this.EventService.findAll();
   }
 
+  @ApiResponse(ApiSuccessResponseHelper(Event.name))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.EventService.findOne(id);
   }
 
+  @ApiResponse(ApiSuccessResponseHelper(CreateEventDto.name))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Put(':id')
   update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
     return this.EventService.update(id, updateEventDto);
   }
 
+  @ApiResponse(ApiSuccessResponseHelper(CreateEventDto.name))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Post(':id/slots')
   addSlot(@Param('id') id: string, @Body() slot: CreateSlotDto) {
     return this.EventService.addSlot(id, slot);
   }
 
+  @ApiResponse(ApiSuccessResponseHelper(CreateEventDto.name))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Post(':id/messages')
   addMessage(@Param('id') id: string, @Body() message: CreateMessageDto) {
     return this.EventService.addMessage(id, message);
   }
 
+  @ApiResponse(ApiSuccessResponseHelper(Event.name))
+  @ApiResponse(ApiNotSucessResponseHelper())
   @Delete(':id')
   deleteOne(@Param('id') id: string) {
     return this.EventService.delete(id);
