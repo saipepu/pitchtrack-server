7a762b3bb490e8998f2eb9ee7add7d6de679156b
Han Htet Aung <juzbird11@gmail.com>
2024-08-19 20:34:55 +0700

    modified organizer schema

--- src/organizer/schema/organizer.schema.ts
+++ src/organizer/schema/organizer.schema.ts
@@ -1,34 +1,32 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 import { Document } from "mongoose";
+import { Role } from "../enums/organizer.enums";
+import { Event } from "src/events/schemas/event.schema";
 
-enum Role {
-  organizer = 'organizer',
-  speaker = 'speaker',
-}
 @Schema({
   timestamps: true,
 })
 export class Organizer extends Document {
-  @ApiProperty({ example: 'Sydny', description: 'Name' })
-  @Prop({ required: [true, "name is required"] })
+  @ApiProperty({ example: 'Organizer1' })
+  @Prop({ required: true})
   name: string;
 
-  @ApiProperty({ example: 'JohnDoe@gmail.com', description: 'Email Address' })
-  @Prop({ required: [true, "email is required"], unique: true })
+  @ApiProperty({ example: 'organizer1@gmail.com'})
+  @Prop({ required: true, unique: true })
   email: string;
 
-  @ApiProperty({ example: 'password', description: 'Password' })
-  @Prop({ required: [true, "password is required"] })
+  @ApiProperty({ example: 'password' })
+  @Prop({ required: true })
   hashedPassword: string;
 
-  @ApiProperty({ example: Role.organizer, description: 'Role' })
-  @Prop({ required: [true, "role is required"], default: "speaker" })
+  @ApiProperty({ example: Role.ORGANIZER, enum: Role })
+  @Prop({ required: true})
   role: Role;
 
-  // eventid list
-  @Prop({ type: [String], default: [] })
-  events: string[];
+  @ApiPropertyOptional({ type: Event, isArray: true})
+  @Prop({ type: [Event] })
+  events: Event[];
 }
 
 export const OrganizerSchema = SchemaFactory.createForClass(Organizer);
