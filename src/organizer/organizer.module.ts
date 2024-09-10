76e7398889732b6f33a77225c12c9a20e6a0a76c
saipepu <saipepu.mdy257@gmail.com>
2024-09-10 14:41:58 +0700

    Added Event Schema

--- src/organizer/organizer.module.ts
+++ src/organizer/organizer.module.ts
@@ -3,15 +3,18 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { OrganizerSchema } from "./schema/organizer.schema";
 import { OrganizerController } from "./organizer.controller";
 import { OrganizerService } from "./organizer.service";
+import { EventService } from "src/events/events.service";
+import { EventSchema } from "src/events/schemas/event.schema";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: 'Organizer', schema: OrganizerSchema }
+      { name: 'Organizer', schema: OrganizerSchema },
+      { name: 'Event', schema: EventSchema }
     ])
   ],
   controllers: [OrganizerController],
-  providers: [OrganizerService],
+  providers: [OrganizerService, EventService],
   exports: [OrganizerService]
 })
 
