e7fcc885e909c581cf81b83ca426ea9c80b5beeb
saipepu <saipepu.mdy257@gmail.com>
2024-09-13 02:46:52 +0700

    Inject Dependency

--- src/organizer/organizer.module.ts
+++ src/organizer/organizer.module.ts
@@ -1,20 +1,23 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { OrganizerSchema } from "./schema/organizer.schema";
 import { OrganizerController } from "./organizer.controller";
 import { OrganizerService } from "./organizer.service";
 import { EventService } from "src/events/events.service";
 import { EventSchema } from "src/events/schemas/event.schema";
+import { TimerGateway } from "src/timer/timers.gateway";
+import { EventsModule } from "src/events/events.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Organizer', schema: OrganizerSchema },
       { name: 'Event', schema: EventSchema }
-    ])
+    ]),
+    forwardRef(() => EventsModule),
   ],
   controllers: [OrganizerController],
-  providers: [OrganizerService, EventService],
+  providers: [OrganizerService, EventService, TimerGateway],
   exports: [OrganizerService]
 })
 
